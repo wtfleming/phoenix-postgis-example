2e3d49ff56116dde3f43ecf215669954416266b5
shavit <shavit@users.noreply.github.com>
2018-03-23 12:44:30 -0400

    Update the page controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule OceanShipLogbooks.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Captain Cook's travels"
   end
 end
